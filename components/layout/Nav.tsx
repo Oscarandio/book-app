f972ab4f601be712632f8e06e6c72ae33ed47257
gvazquez-persuadis <gvazquez@persuadis.com>
2024-08-09 10:55:48 +0200

    añadido menu responsive

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Iconos de hamburguesa y cerrar
 
 interface NavProps {}
 
 const Nav: React.FC<NavProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='bg-jacaranda'>
-      <nav className='container mx-auto h-16 flex justify-between items-center px-3'>
+      <nav className='container mx-auto h-16 flex justify-between items-center px-3 md:px-0'>
         <div>
           <Link href='/' aria-label='Home'>
             LOGO
           </Link>
         </div>
-        <ul className='flex space-x-16'>
-          <li>
+        <div className='md:hidden'>
+          {/* Icono hamburguesa */}
+          <button onClick={toggleMenu} aria-label='Toggle Menu'>
+            {isOpen ? <FaTimes size={24} /> : <FaBars size={28} />}
+          </button>
+        </div>
+        {/* Menú principal */}
+        <ul
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } md:flex md:space-y-0 md:space-x-16 absolute md:relative top-16 md:top-0 left-0 space-y-4 w-full md:w-auto bg-jacaranda md:bg-transparent z-10 md:z-auto flex flex-col md:flex-row p-3 md:p-0 le text-2xl md:text-base font-medium`}>
+          <li className='py-2'>
             <Link href='/'>Home</Link>
           </li>
-          <li>
+          <li className='py-2'>
             <Link href='/categories'>Categorias</Link>
           </li>
-          <li>
+          <li className='py-2'>
             <Link href='/lists'>Listas</Link>
           </li>
         </ul>
